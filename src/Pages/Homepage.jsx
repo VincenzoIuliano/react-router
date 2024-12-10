cf7a99f53296e8f70dbc91fe8401a90e7ed2177b
VincenzoIuliano <bubucane@outlook.it>
2024-12-10 15:54:51 +0100

    ADD: main files + homepage

---
+++ src/Pages/Homepage.jsx
@@ -0,0 +1,11 @@
+export default function Homepage() {
+    return (
+      <main>
+        <section>
+          <h1>Queste è la home page</h1>
+  
+          
+        </section>
+      </main>
+    )
+  }
